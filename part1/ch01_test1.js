66211b763a6253f041d68c3255644daea12572c9
kne2317 <skdms2317@gmail.com>
2021-05-27 00:18:59 +0900

    duration_sum

---
+++ part1/ch01_test1.js
@@ -0,0 +1,13 @@
+var result = 0;
+
+console.time('duration_sum');
+
+for (var i =1; i<=1000; i++){
+    result+=i;
+}
+
+console.timeEnd('duration_sum');
+console.log('1부터 1000까지 더한 결과물 : %d', result);
+
+
+//duration_sum은 시작부터 끝까지의 시간을 측정한다.
