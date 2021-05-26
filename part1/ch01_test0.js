5e10841e62a301e642ea4b3b90787a946b22ea03
kne2317 <skdms2317@gmail.com>
2021-05-27 00:13:56 +0900

    prac1

--- part1/ch01_test0.js
+++ part1/ch01_test0.js
@@ -1,1 +1,9 @@
 console.log('안녕하세요');
+
+console.log('결과는 %d입니다.',10);
+
+console.log('숫자 보여주기 : %d',10);
+
+console.log('문자열 보여주기 : %s',"node.js 학습하기");
+
+console.log('JSON 객체 보여주기 : %j',{lang:'node.js'});
